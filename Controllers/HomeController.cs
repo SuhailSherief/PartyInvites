78bad09ff2057c3e10f669600a225d01569c3a58
suhai <suhai@LUCIFER>
2022-04-17 19:40:51 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+}
